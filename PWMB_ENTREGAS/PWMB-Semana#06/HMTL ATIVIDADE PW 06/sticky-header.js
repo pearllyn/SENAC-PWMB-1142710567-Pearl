a3541c4e7bb5f502023536554a7154bc785203e4
Pearl <142231990+pearllyn@users.noreply.github.com>
2023-11-12 21:28:18 -0300

    Delete PWMB_ENTREGAS/PWMB-Semana#06/HMTL ATIVIDADE PW 06 directory

--- PWMB_ENTREGAS/PWMB-Semana#06/HMTL ATIVIDADE PW 06/sticky-header.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener("scroll", () => {
-    const header = document.querySelector("header");
-    if (window.pageYOffset > 100) {
-    header.classList.add("sticky");
-    } else {
-    header.classList.remove("sticky");
-    }
-    });
